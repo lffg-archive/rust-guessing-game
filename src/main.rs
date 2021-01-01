57d981856390773e4cda484f4a41ea2ddc8e4c89
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-01-01 18:31:43 -0300

    Finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io::{self, Write};
+
+fn read_line(prompt: &str) -> String {
+    print!("{} ", prompt);
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    input
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rust guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("Your secret number: {}.", secret_number);
+
+    loop {
+        let guess: isize = match read_line("Enter the number:").trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("You must provide a valid number.");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("Right guess, you won! :)");
+                break;
+            }
+        };
+    }
 }
